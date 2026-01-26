155c62d11eceea21ac0ba730967ef4a744c4d43b
javier-anton-ordonez <javierantonordonez@gmail.com>
2026-01-26 12:25:34 +0100

    Pelotazo urbanistico

--- acepta-el-reto/Pelotazo-urbanistico.c
+++ acepta-el-reto/Pelotazo-urbanistico.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 long long maximo_comun_divisor(long long a, long long b) {
-  long long temporal; // Para no perder b
+  long long temporal;
   while (b != 0) {
     temporal = b;
     b = a % b;
@@ -12,26 +12,26 @@ long long maximo_comun_divisor(long long a, long long b) {
 
 void casas(long long w, long long h) {
   long long minimo = 1;
+
   if (w == h) {
     printf("%lld\n", minimo);
     return;
   }
-  long long mcd = maximo_comun_divisor(w, h);
-  long long x = mcd;
-  long long x2 = x * x;
-  long long area = (long)w * (long)h;
-  minimo = area / x2;
-  printf("%lld\n", minimo);
 
-  // printf("X: %d\n", x);
-  // printf("X2: %d\n", x2);
-  // printf("MCD: %d\n", mcd);
-  // printf("tmp: %d\n", tmp);
-  // printf("W * H: %lld\n", area);
+  long long mcd = maximo_comun_divisor(w, h);
+
+  long long x = mcd;
+  long long x2 = x * x;   // ya no overflow
+  long long area = w * h; // ya no overflow
+
+  minimo = area / x2;
+
+  printf("%lld\n", minimo);
 }
 
 int main() {
   int numCasos;
+
   if (scanf("%d", &numCasos) == 1) {
     while (numCasos--) {
       long long w, h;
