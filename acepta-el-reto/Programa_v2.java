0e9f73841fa208d547576e3aa8db5aaef47f7918
javier-anton-ordonez <javierantonordonez@gmail.com>
2026-02-13 20:07:35 +0100

    Funciona pero no de tiempo

--- acepta-el-reto/Programa_v2.java
+++ acepta-el-reto/Programa_v2.java
@@ -1,87 +1,122 @@
 import java.io.*;
 import java.util.*;
 
-class Posicion {
-  public int Valor;
-  public int I;
-  public int J;
+class infoTablero {
+  boolean[] Columnas;
+  boolean[] Filas;
 
-  public Posicion(int Valor, int I, int J) {
-    this.I = I;
-    this.J = J;
-    this.Valor = Valor;
+  public infoTablero(int N) {
+    this.Filas = new boolean[N];
+    this.Columnas = new boolean[N];
   }
 }
 
 public class Programa_v2 {
+  static int mejorGlobal;
 
-  static boolean diagonalLibre(int[][] tablero, int i, int j) {
-    int N = tablero.length;
-    // Diagonal superior izquierda
-    for (int x = i - 1, y = j - 1; x >= 0 && y >= 0; x--, y--)
-      if (tablero[x][y] == -1)
-        return false;
-    // Diagonal superior derecha
-    for (int x = i - 1, y = j + 1; x >= 0 && y < N; x--, y++)
-      if (tablero[x][y] == -1)
-        return false;
-    // Diagonal inferior izquierda
-    for (int x = i + 1, y = j - 1; x < N && y >= 0; x++, y--)
-      if (tablero[x][y] == -1)
-        return false;
-    // Diagonal inferior derecha
-    for (int x = i + 1, y = j + 1; x < N && y < N; x++, y++)
-      if (tablero[x][y] == -1)
-        return false;
+  static boolean diagonalLibre(boolean[][] esReina, int i, int j, int N) {
+    /*
+     * // Diagonal superior izquierda
+     * for (int x = i - 1; x >= 0; x--) {
+     * for (int y = j - 1; y >= 0; y--) {
+     * if (esReina[x][y])
+     * return false;
+     * }
+     * for (int y = j + 1; y < N; y++) {
+     * if (esReina[x][y])
+     * return false;
+     * }
+     * 
+     * }
+     * 
+     * // Diagonal inferior derecha
+     * for (int x = i + 1; x < N; x++) {
+     * for (int y = j - 1; y >= 0; y--) {
+     * if (esReina[x][y])
+     * return false;
+     * }
+     * 
+     * for (int y = j + 1; y < N; y++) {
+     * if (esReina[x][y])
+     * return false;
+     * }
+     * }
+     */
+    int radio = 1;
 
+    int salido[] = { 0, 0, 0, 0 };
+
+    N = N - 1;
+    do {
+
+      if (i - radio < 0) {
+        salido[0] = 1;
+      }
+
+      if (i + radio > N) {
+        salido[1] = 1;
+      }
+
+      if (j + radio > N) {
+        salido[2] = 1;
+      }
+
+      if (j - radio < 0) {
+        salido[3] = 1;
+      }
+
+      if (esReina[Math.max(i - radio, 0)][Math.max(j - radio, 0)]) {
+        return false;
+      }
+      if (esReina[Math.max(i - radio, 0)][Math.min(j + radio, N)]) {
+        return false;
+      }
+      // System.out.println(Math.min(i + radio, N));
+      // System.out.println(Math.max(j - radio, 0));
+      if (esReina[Math.min(i + radio, N)][Math.max(j - radio, 0)]) {
+        return false;
+      }
+      if (esReina[Math.min(i + radio, N)][Math.min(j + radio, N)]) {
+        return false;
+      }
+      radio++;
+    } while (salido[0] + salido[1] + salido[2] + salido[3] < 4);
     return true;
   }
 
-  public static void Torres_y_Reinas(int[][] tablero, int Q, int R, int valor) {
-    int Final = 0;
-    List<Integer> DelRow = new ArrayList<>();
-    List<Integer> DelCol = new ArrayList<>();
-    List<Posicion> Posiciones = new ArrayList<>();
-    for (int j = 0; j < Q + R; j++) {
-      for (int i = 0; i < Q + R; i++) {
-        Posicion tmp = new Posicion(tablero[i][j], i, j);
-        Posiciones.add(tmp);
-      }
+  public static void Torres_y_Reinas(int[][] tablero, boolean[][] esReina, int Q, int R, int valor, infoTablero info) {
+    if (Q == 0 && R == 0) {
+      mejorGlobal = Math.max(mejorGlobal, valor);
+      return;
     }
-    Collections.sort(Posiciones, new Comparator<Posicion>() {
-      @Override
-      public int compare(Posicion a, Posicion b) {
-        return b.Valor - a.Valor;
-      }
-    });
 
-    int index = 0;
-    while (Q + R > 0 && index < tablero.length * tablero.length) {
-      if (!DelRow.contains(Posiciones.get(index).I) && !DelCol.contains(Posiciones.get(index).J)) {
-        if (R > 0) {
+    int N = tablero.length;
 
-          R--;
-          DelRow.add(Posiciones.get(index).I);
-          DelCol.add(Posiciones.get(index).J);
-          tablero[Posiciones.get(index).I][Posiciones.get(index).J] = -1;
-          Final += Posiciones.get(index).Valor;
-        } else {
-          if (diagonalLibre(tablero, Posiciones.get(index).I, Posiciones.get(index).J)) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
 
-            DelRow.add(Posiciones.get(index).I);
-            DelCol.add(Posiciones.get(index).J);
-            tablero[Posiciones.get(index).I][Posiciones.get(index).J] = -1;
-            Q--;
-            Final += Posiciones.get(index).Valor;
-          }
+        if (Q > 0 && !info.Filas[i] && !info.Columnas[j] && diagonalLibre(esReina, i, j, N)) {
+          info.Columnas[j] = true;
+          info.Filas[i] = true;
+          esReina[i][j] = true;
+          Torres_y_Reinas(tablero, esReina, Q - 1, R, valor + tablero[i][j], info);
+
+          info.Columnas[j] = false;
+          info.Filas[i] = false;
+          esReina[i][j] = false;
+        }
+
+        if (R > 0 && !info.Filas[i] && !info.Columnas[j] && diagonalLibre(esReina, i, j, N)) {
+          info.Columnas[j] = true;
+          info.Filas[i] = true;
+
+          Torres_y_Reinas(tablero, esReina, Q, R - 1, valor + tablero[i][j], info);
+
+          info.Columnas[j] = false;
+          info.Filas[i] = false;
         }
       }
-      index++;
     }
-    if (R > 0 || Q > 0) {
-      Final = 0;
-    }
-    System.out.println(Final);
   }
 
   public static void main(String[] args) throws Exception {
@@ -96,19 +131,27 @@ public class Programa_v2 {
       StringTokenizer st = new StringTokenizer(line);
       int Q = Integer.parseInt(st.nextToken());
       int R = Integer.parseInt(st.nextToken());
-
       int N = Q + R;
 
       int[][] tablero = new int[N][N];
-
       for (int i = 0; i < N; i++) {
         st = new StringTokenizer(br.readLine());
         for (int j = 0; j < N; j++) {
           tablero[i][j] = Integer.parseInt(st.nextToken());
         }
       }
-      Torres_y_Reinas(tablero, Q, R, 0);
+      if (tablero.length == 2 && Q >= 1 && R > 0) {
+        System.out.println(0);
+        return;
+      }
+
+      mejorGlobal = 0;
+      infoTablero info = new infoTablero(N);
+      boolean[][] esReina = new boolean[N][N];
+
+      Torres_y_Reinas(tablero, esReina, Q, R, 0, info);
+
+      System.out.println(mejorGlobal);
     }
   }
-
 }
